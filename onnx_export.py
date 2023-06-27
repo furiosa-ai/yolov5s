adec39591b77306a767962042f51dba40170dc03
jongwook_kim <134344843+jongwook11@users.noreply.github.com>
2023-06-27 15:53:24 +0900

    Add converting openmmlab torch model to onnx

--- onnx_export.py
+++ onnx_export.py
@@ -1,7 +1,21 @@
 import argparse 
 import torch
 
+from mmengine.config import Config, ConfigDict, DictAction
+from mmengine.runner import Runner
+from mmyolo.registry import RUNNERS
+
 INPUT_SHAPE = (640, 640)
+
+def load_openmmlab_model(config, weights):
+    cfg = Config.fromfile(config)
+    cfg.load_from = weights
+    cfg.test_pipeline[1]['scale'] = INPUT_SHAPE
+
+    runner = Runner.from_cfg(cfg)
+    runner.call_hook('before_run')
+    runner.load_or_resume()
+    return runner.model
 
 def main():
     args = build_argument_parser()
@@ -10,8 +24,18 @@ def main():
     opset = args.opset_version
     input_names = args.model_input_name
     output_names = args.model_output_name
+    model_repo = args.model_repo
 
-    torch_model = torch.hub.load("ultralytics/yolov5:v7.0", "yolov5s")
+    assert model_repo in ["ultralytics", "openmmlab"], "Unsupported torchmodel for yolov5s structure!!"
+
+    if model_repo == "ultralytics":
+        torch_model = torch.hub.load("ultralytics/yolov5:v7.0", "yolov5s")
+    elif model_repo == "openmmlab":
+        config = args.config
+        weights = args.weights
+        #assert config and weights, "Config and Weight files are essential for converting openmmlab model!!"
+        torch_model = load_openmmlab_model(config, weights)
+
     torch_model = torch_model.eval()
 
     print("Start ONNX export...")
@@ -23,13 +47,15 @@ def main():
         output_names = [output_names],
         opset_version = opset,
     )
-
     print(f"Completed ONNX export >> {onnx_model_path}")
 
 
 def build_argument_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--onnx_path',type=str, default = "./Result.onnx")
+    parser.add_argument('--model_repo', type=str, default = "ultralytics", help = "repository = [ultralytics, openmmlab]")
+    parser.add_argument('--config', type = str, default = "./datas/yolov5_s-v61_syncbn_fast_8xb16-300e_coco.py", help = "Path to config file to convert openmmlab torch model")
+    parser.add_argument('--weights', type = str, default = "./datas/yolov5_s-v61_syncbn_fast_8xb16-300e_coco_20220918_084700-86e02187.pth", help = "Path to weight file to convert openmmlab torch model")
     parser.add_argument('--opset_version', type=int, default=13, help='the ONNX version to export the model to') 
     parser.add_argument('--model_input_name', type=str, default='images', help="the model's input name") 
     parser.add_argument('--model_output_name', type=str, default='output', help="the model's output name")
