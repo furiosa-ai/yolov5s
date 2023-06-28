9a5d2fadb73bd8a54ba83c111a4f888ba485b4b5
jongwook_kim <134344843+jongwook11@users.noreply.github.com>
2023-06-28 15:58:13 +0900

    Style black

--- furiosa_quantize.py
+++ furiosa_quantize.py
@@ -12,6 +12,7 @@ from utils.preprocess import load_input
 
 INPUT_SHAPE = (640, 640)
 
+
 def main():
     args = build_argument_parser()
 
@@ -23,22 +24,27 @@ def main():
     input_name = args.model_input_name
     model_repo = args.model_repo
 
-    assert model_repo in ["ultralytics", "openmmlab"], "Unsupported torchmodel for yolov5s structure!!"
-    
+    assert model_repo in [
+        "ultralytics",
+        "openmmlab",
+    ], "Unsupported torchmodel for yolov5s structure!!"
+
     f32_onnx_model = onnx.load_model(onnx_path)
     extracted_onnx_model = extract_model(f32_onnx_model, input_name, model_repo)
-    optimized_onnx_model = optimize_model(model = extracted_onnx_model, opset_version=opset, input_shapes = {input_name: [1,3,*INPUT_SHAPE]})
+    optimized_onnx_model = optimize_model(
+        model=extracted_onnx_model,
+        opset_version=opset,
+        input_shapes={input_name: [1, 3, *INPUT_SHAPE]},
+    )
 
     calib_data_names = os.listdir(calib_data)
-    calib_data_names = random.choices(calib_data_names, k = calib_count)
+    calib_data_names = random.choices(calib_data_names, k=calib_count)
 
     calibrator = Calibrator(optimized_onnx_model, CalibrationMethod.SQNR_ASYM)
 
-    for image_name in tqdm(
-        calib_data_names, desc="Calibrating", unit="image", mininterval=0.5
-    ):
+    for image_name in tqdm(calib_data_names, desc="Calibrating", unit="image", mininterval=0.5):
         image_path = os.path.join(calib_data, image_name)
-        image, _ = load_input(image_path, new_shape = INPUT_SHAPE)
+        image, _ = load_input(image_path, new_shape=INPUT_SHAPE)
         calibrator.collect_data([[image]])
 
     ranges = calibrator.compute_range()
@@ -50,14 +56,15 @@ def main():
 
     print(f"Completed quantinization >> {dfg_path}")
 
+
 def extract_model(model: onnx.ModelProto, input_name, model_repo):
     """Cut off the post-processing components."""
-    input_to_shape = [(input_name, (1,3, *INPUT_SHAPE))]
+    input_to_shape = [(input_name, (1, 3, *INPUT_SHAPE))]
 
     # TODO: The cut points below were chosen so that the postprocess() function can follow the
     # structure of the original PyTorch code as closely as possible. We need to experiment more with
     # different cut points to achieve the best combination of speed and accuracy.
-    
+
     if model_repo == "ultralytics":
         output_to_shape = (
             # between /model/model/model.24/m.0/Conv and /model/model/model.24/Reshape
@@ -69,11 +76,8 @@ def extract_model(model: onnx.ModelProto, input_name, model_repo):
         )
     else:
         output_to_shape = (
-            # between /model/model/model.24/m.0/Conv and /model/model/model.24/Reshape
             ("/head_module/convs_pred.0/Conv_output_0", (1, 255, 80, 80)),
-            # between /model/model/model.24/m.1/Conv and /model/model/model.24/Reshape_2
             ("/head_module/convs_pred.1/Conv_output_0", (1, 255, 40, 40)),
-            # between /model/model/model.24/m.2/Conv and /model/model/model.24/Reshape_4
             ("/head_module/convs_pred.2/Conv_output_0", (1, 255, 20, 20)),
         )
     input_to_shape = {
@@ -102,17 +106,41 @@ def extract_model(model: onnx.ModelProto, input_name, model_repo):
 
     return extracted_model
 
+
 def build_argument_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--onnx_path', type=str, default = "Result.onnx", help = 'Path to onnx file') # onnx 
-    parser.add_argument('--model_repo', type=str, default = "ultralytics", help = "repository = [ultralytics, openmmlab]")
-    parser.add_argument('--dfg_path', type = str, default = "./Result.dfg", help = "Path to i8 onnx file") # dfg file path
-    parser.add_argument('--opset_version', type=int, default=13, help='the ONNX version to export the model to') 
-    parser.add_argument('--calib_data', type = str, default = "./coco/train2017", help = "Path to calibration data containing image files") # calibration dataset path
-    parser.add_argument('--calib_count', default=10, type=int, help="How many images to use for calibration") # number of calibration data
-    parser.add_argument('--model_input_name', type=str, default='images', help="the model's input name") 
+    parser.add_argument("--onnx_path", type=str, default="Result.onnx", help="Path to onnx file")
+    parser.add_argument(
+        "--model_repo",
+        type=str,
+        default="ultralytics",
+        help="repository = [ultralytics, openmmlab]",
+    )
+    parser.add_argument("--dfg_path", type=str, default="./Result.dfg", help="Path to i8 onnx file")
+    parser.add_argument(
+        "--opset_version",
+        type=int,
+        default=13,
+        help="the ONNX version to export the model to",
+    )
+    parser.add_argument(
+        "--calib_data",
+        type=str,
+        default="./coco/train2017",
+        help="Path to calibration data containing image files",
+    )
+    parser.add_argument(
+        "--calib_count",
+        default=10,
+        type=int,
+        help="How many images to use for calibration",
+    )
+    parser.add_argument(
+        "--model_input_name", type=str, default="images", help="the model's input name"
+    )
     args = parser.parse_args()
     return args
+
 
 if __name__ == "__main__":
     main()
